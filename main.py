a90583a6f517b341ba90ae0bf8ce234ad384482a
DylanMarn <dylannkmarn@gmail.com>
2022-11-05 11:27:08 -0700

    Outline Pseudocode

---
+++ main.py
@@ -0,0 +1,19 @@
+# Dylan Marn
+
+from flask_sqlalchemy import SQLAlchemy
+
+# Create a data base that saves a users login and cash amount
+
+# Create a dictionary of 52 cards in deck and a list of random numbers between 1 and 52 that can only appear once
+
+# Create a function that lets users decide next move
+
+# Create a bet amount function
+
+# Create a "hit" function
+
+# Create a "stand" function
+
+# Create a "double down" function
+
+# Create a "split" function
